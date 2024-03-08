3afbef25de02faa584f9ad4268b6cb311390602b
ajmal-j <ajmaljaleel7860@gmail.com>
2024-03-09 00:59:41 +0530

    Export password util

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export * from "./utils/express-callback";
 export * from "./utils/express-response";
 export * from "./utils/token";
-
+export * from "./utils/password";
 export * from "./errorHandler";
 export * from "./middlewares/error.middleware";
 export * from "./middlewares/verifyUser.middleware";
