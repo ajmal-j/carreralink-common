eb5f41e7b230f8793f73fd46b9222a4945ee083d
ajmal-j <ajmaljaleel7860@gmail.com>
2024-03-18 18:17:51 +0530

    Updated jtw funcitons

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,14 +1,12 @@
 import jwt, { JsonWebTokenError } from "jsonwebtoken";
 import { UnauthorizedError } from "../errorHandler";
 
-export const generateToken = (data: Object): string => {
-  let secret = process.env.JWT_SECRET as string;
+export const generateToken = (data: Object, secret: string): string => {
   if (!secret) throw new Error("JWT_SECRET is missing");
-  return jwt.sign(data, secret);
+  return jwt.sign(data, secret, { expiresIn: "30d" });
 };
 
-export const decodeToken = (token: string): Object => {
-  let secret = process.env.JWT_SECRET as string;
+export const decodeToken = (token: string, secret: string): Object => {
   if (!secret) throw new Error("JWT_SECRET is missing");
   try {
     const data = token.split(" ")[1];
